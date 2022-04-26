a45425de22942ded776c607b358bc688bdfb44aa
PeterTakahashi <seiya4@icloud.com>
2022-04-26 17:31:54 +0900

    feat: prettier target without locale/js

--- locale/ja.js
+++ locale/ja.js
@@ -1,13 +1,1 @@
-/*eslint-disable*/ module.exports = {
-  messages: {
-    'Blockchain lottery building service for municipalities.': '自治体向けのブロックチェーン宝くじ構築サービス',
-    Dashboard: 'ダッシュボード',
-    LEGAL: '法律',
-    Language: '言語',
-    Lottery: '宝くじ',
-    Markting: 'マーケティング',
-    SOLUTIONS: 'ソリューション',
-    SUPPORT: 'サポート',
-    test: 'テスト',
-  },
-}
+/*eslint-disable*/module.exports={messages:{"Blockchain lottery building service for municipalities.":"自治体向けのブロックチェーン宝くじ構築サービス","Dashboard":"ダッシュボード","LEGAL":"法律","Language":"言語","Lottery":"宝くじ","Markting":"マーケティング","SOLUTIONS":"ソリューション","SUPPORT":"サポート","test":"テスト"}};
