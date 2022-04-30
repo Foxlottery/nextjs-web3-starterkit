dd09a1e3b55f439e51bc13142c9bf192ceeaa432
PeterTakahashi <seiya4@icloud.com>
2022-04-30 19:50:16 +0900

    feat: fix page design

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -5,7 +5,13 @@ import type { NextPage } from 'next'
 const Dashboard: NextPage = () => {
   const { i18n } = useLingui()
 
-  return <>{i18n._(t`Dashboard`)}</>
+  return (
+    <>
+      <div className="px-5 py-5 border-b border-gray-200 w-80">
+        <h3 className="text-lg font-medium leading-6 text-gray-900">{i18n._(t`Dashboard`)}</h3>
+      </div>
+    </>
+  )
 }
 
 export default Dashboard
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { t } from '@lingui/macro'
 import { useLingui } from '@lingui/react'
 import { Feature } from 'app/enums'
 import NetworkGuard from 'app/guards/Network'
@@ -6,32 +7,8 @@ const Home = () => {
   const { i18n } = useLingui()
   return (
     <>
-      <div className="my-10 shadow bg-gray-50 sm:rounded-lg">
-        <div className="px-4 py-5 sm:p-6">
-          <div>
-            <label htmlFor="price" className="block text-sm font-medium text-gray-700">
-              Price
-            </label>
-            <div className="relative mt-5 rounded-md shadow-sm">
-              <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                <span className="text-gray-500 sm:text-sm">$</span>
-              </div>
-              <input
-                type="text"
-                name="price"
-                id="price"
-                className="block w-full pr-12 border-gray-300 rounded-md focus:ring-green-500 focus:border-green-500 pl-7 sm:text-sm"
-                placeholder="0.00"
-                aria-describedby="price-currency"
-              />
-              <div className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
-                <span className="text-gray-500 sm:text-sm" id="price-currency">
-                  USD
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
+      <div className="px-5 py-5 border-b border-gray-200 w-80">
+        <h3 className="text-lg font-medium leading-6 text-gray-900">{i18n._(t`Lottery`)}</h3>
       </div>
     </>
   )
